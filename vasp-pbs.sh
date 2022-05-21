49170f0f6bd1bed5150b5882ce3d25eb6da64fc9
zhangtingfeng <87751217+tingfengzhang@users.noreply.github.com>
2022-05-21 17:15:29 +0800

    Update vasp-pbs.sh

--- vasp-pbs.sh
+++ vasp-pbs.sh
@@ -2,7 +2,7 @@
 
 touch vasp.pbs
 
-echo '#!/bin/bash -x
+echo  -e "#!/bin/bash -x
 
 #PBS -N mpi
 #PBS -l nodes=2:ppn=24
@@ -10,18 +10,17 @@ echo '#!/bin/bash -x
 #PBS -e mpi.err
 #PBS -q batch
 #define variables
-echo $PBS_O_WORKDIR
-NCPUS=`wc -l $PBS_NODEFILE | awk '{print $1}'`
-echo $NCPUS
+echo \$PBS_O_WORKDIR
+NCPUS=\`wc -l \$PBS_NODEFILE | awk '{print \$1}'\`
+echo \$NCPUS
 #Run your executable
 #echo starting
 #date
-cd $PBS_O_WORKDIR
+cd \$PBS_O_WORKDIR
 
-PAR="-np $NCPUS -machinefile $PBS_NODEFILE"
+PAR=\"-np \$NCPUS -machinefile \$PBS_NODEFILE\"
 VASP=/public/sourcecode/apps/VASP/vasp.6.1.0-varsoc-varopt-wanner90_1.2/bin/vasp_std
 logfile=runlog
 
-run="mpirun $PAR $VASP"
-$run >runlog
-' > vasp.pbs
+run=\"mpirun \$PAR \$VASP\"
+\$run >runlog" > vasp.pbs
